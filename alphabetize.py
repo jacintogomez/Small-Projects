3e0963729002a307d4d0b973449ed2f64af5673b
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-07-01 19:44:34 -0400

    Remove duplicated lines

--- alphabetize.py
+++ alphabetize.py
@@ -1,9 +1,9 @@
 def alphabetize_it(filename):
     with open(filename,'r') as file:
         items=[line.strip() for line in file if line.strip()]
-    items.sort()
+    unique_items=sorted(set(items))
     with open(filename,'w') as file:
-        for item in items:
+        for item in unique_items:
             file.write(item+'\n')
 
 fname=input('Enter filename: ')
