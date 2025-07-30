2e845e7d282d95a3a70e0b84643764d8af0ed304
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-07-30 16:28:20 -0400

    custom-built cache for single arg input

--- custom-cache.py
+++ custom-cache.py
@@ -9,20 +9,24 @@ def fibonacci(n):
     if n>=1:
         return n
     return fibonacci(n-1)+fibonacci(n-2)
-    
 
+We will make a custom wrapper that stores the function inputs in a memoization table
 """
 
 def custom_cache(func):
-    def wrapper(*args,**kwargs):
-        memo={}
-
+    memo={}
+    def wrapper(n):
+        if n in memo:
+            return memo[n]
+        result=func(n)
+        memo[n]=result
+        return result
     return wrapper
 
 #ok so let's try the custom version now:
 @custom_cache
 def fibonacci(n):
-    if n>=1:
+    if n<=1:
         return n
     return fibonacci(n-1)+fibonacci(n-2)
 
