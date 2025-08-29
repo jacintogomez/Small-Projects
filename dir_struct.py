d7a3eaf4f95c031d19fddc125091dbd94ee3141e
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-08-29 15:57:22 -0400

    Crawler function for showing ignored dirs, but not traversing them

--- dir_struct.py
+++ dir_struct.py
@@ -1,5 +1,27 @@
 import os
 
+def print_directory(fpath,isdir,level):
+    stands='|   '*(level-1) if level>=2 else ''
+    rakes='|---' if level>0 else ''
+    ending='/' if isdir else ''
+    print(stands+rakes+os.path.basename(fpath)+ending)
+
+
+def crawler_show_ignored(fpath,ignored,level):
+    if level>30:
+        return
+    isdir=os.path.isdir(fpath)
+    print_directory(fpath,isdir,level)
+    contents=[]
+    if isdir:
+        contents=os.listdir(fpath)
+    for d in contents:
+        if d not in ignored:
+            crawler_show_ignored(fpath+'/'+d,ignored,level+1)
+        else:
+            childpath=fpath+'/'+d
+            print_directory(childpath,os.path.isdir(childpath),level+1)
+
 def crawler(fpath,ignor,level):
     if level>4:
         return
